5c3b3102164653b39c9351bd961f1735282a1cab
Jenubis78 <74786409+Jenubis78@users.noreply.github.com>
2023-02-21 11:54:51 +0100

    Remove original css + no more error NavMenu

--- Shop/Client/Services/CategoryService/CategoryService.cs
+++ Shop/Client/Services/CategoryService/CategoryService.cs
@@ -8,7 +8,7 @@
         {
             _http = http;
         }
-        public List<Category> Categories { get; set; }
+        public List<Category> Categories { get; set; } = new List<Category>();
         public async Task GetCategories()
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
